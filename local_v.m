284aa088432ea4d510d9f2c69e478e2f4ecc00fc
anozadze <94999148+anozadze@users.noreply.github.com>
2021-12-22 22:55:22 +0100

    Update local_v.m

--- local_v.m
+++ local_v.m
@@ -4,7 +4,7 @@ function v_local = local_v(vp,c_eff,c_plat,v0)
 % "v(pp)") of particle pp based on the effective concentration v_eff "how
 % fast is the particle actually?", the plateau concentration (if we want a
 % plateau in the profile) and a certain scaling concentration v0.
-% only used for Aerotaxis.
+% only used for Chemokinesis.
 
 %%note that the plateau was rejected during the process.
 
